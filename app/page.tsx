f7fa461cb646268963274f1686c3b8cdc0697248
Devloper-solaiman <devlopersolaiman@gmail.com>
2025-01-08 09:46:28 +0600

    publish now

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
+import { Button } from "@nextui-org/button";
 
 import { siteConfig } from "@/config/site";
 import { GithubIcon } from "@/components/icons";
@@ -10,8 +11,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-       <h1>this is a good sugiation</h1>
-       
+        <Button>pay Now</Button>
       </div>
 
       <div className="flex gap-3">
